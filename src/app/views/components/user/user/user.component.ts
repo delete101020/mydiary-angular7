45cddc5218e2284533f2fbb097e82f27e1c7b784
Kingfisher <delete101020@live.com>
2019-06-25 22:39:52 +0700

    User List

--- src/app/views/components/user/user/user.component.ts
+++ src/app/views/components/user/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../../../../auth/auth.service';
+import { Config, IUser } from '../../../../shared/models';
+import { DataService } from '../../../../shared/services';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  canView = false;
+  canDelete = false;
+
+  config: Config = { table: 'user' };
+  list$: Observable<IUser[]>;
+
+  constructor(
+    private authService: AuthService,
+    private dataService: DataService
+  ) { }
 
   ngOnInit() {
+    this.canDoIt();
+    this.getList();
+  }
+
+  getList() {
+    this.list$ = this.dataService.getList(this.config);
+  }
+
+  canDoIt() {
+    const url = '/' + this.config.table + '/';
+    this.canView = this.authService.checkRole(url + 'detail/');
+    this.canDelete = this.authService.checkRole(url + 'delete');
+  }
+
+  deleteElement(id: number) {
+    if (confirm('Are you sure to delete this record ?')) {
+      this.config.id = id;
+      this.dataService.deleteData(this.config)
+        .subscribe(() => this.getList());
+    }
   }
 
 }
